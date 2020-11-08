204edf5d123a1adf7517286ecd4ec997344490e2
ltann <leeroyleeroy18@gmail.com>
2020-11-08 14:46:14 -0800

    for loop practice

--- code/for.go
+++ code/for.go
@@ -3,13 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	// for i := 1; i <= 100; i++ {
+	// While loop
+	// x := 2
+	// for x <= 5 {
+	// 	println(x)
+	// 	x++
+	// }
+
+	// Traditional for loop
+	// for i := 1; i <= 10; i += 2 {
 	// 	fmt.Println(i)
 	// }
 
-	var mySentence = "This is a sentence"
+	for y := -10; y <= 100; y++ {
+		if y < 0 {
+			println("Negative number")
+			continue
+		}
 
-	for index, letter := range mySentence {
-		fmt.Println("Index:", index, "Letter:", string(letter))
+		if y%3 == 0 && y%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if y%3 == 0 {
+			fmt.Println("Fizz")
+		} else if y%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			println(y)
+		}
 	}
+
+	//
+	// var mySentence = "This is a sentence"
+
+	// for index, letter := range mySentence {
+	// 	fmt.Println("Index:", index, "Letter:", string(letter))
+	// }
 }
